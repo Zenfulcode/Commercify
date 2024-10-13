42a5c2bffc7a3b02e135b3c493ec0c2dbc632b9a
GustavH <gkhaavik@gmail.com>
2024-10-13 19:24:32 +0200

    - adds paginations to user management

--- UserService/src/main/java/com/gostavdev/commercify/userservice/service/UserManagementService.java
+++ UserService/src/main/java/com/gostavdev/commercify/userservice/service/UserManagementService.java
@@ -1,18 +1,16 @@
 package com.gostavdev.commercify.userservice.service;
 
 import com.gostavdev.commercify.userservice.UserRepository;
-import com.gostavdev.commercify.userservice.api.requests.RegisterUserRequest;
 import com.gostavdev.commercify.userservice.dto.UserDTO;
 import com.gostavdev.commercify.userservice.dto.mappers.UserDTOMapper;
 import com.gostavdev.commercify.userservice.entities.UserEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,10 +26,8 @@ public class UserManagementService {
     }
 
     @Transactional(readOnly = true)
-    public List<UserDTO> getAllUsers() {
-        return userRepository.findAll().stream()
-                .map(mapper)
-                .collect(Collectors.toList());
+    public Page<UserDTO> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable).map(mapper);
     }
 
     @Transactional
@@ -81,12 +77,5 @@ public class UserManagementService {
         UserEntity updatedUser = userRepository.save(user);
 
         return mapper.apply(updatedUser);
-    }
-
-    @Transactional(readOnly = true)
-    public UserDTO getUserByEmail(String userEmail) {
-        return userRepository.findByEmail(userEmail)
-                .map(mapper)
-                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
