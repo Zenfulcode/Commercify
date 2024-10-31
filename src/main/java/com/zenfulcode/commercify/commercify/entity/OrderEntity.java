8e45703c543ad22cef6c6d7f9484cfaa874eff03
GustavH <gkhaavik@gmail.com>
2024-10-31 15:49:50 +0100

    Renames tables to be Pascal Case and Adding Stripe secret

--- src/main/java/com/zenfulcode/commercify/commercify/entity/OrderEntity.java
+++ src/main/java/com/zenfulcode/commercify/commercify/entity/OrderEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
+@Table(name = "Orders")
 @NoArgsConstructor
 @Data
 public class OrderEntity {
--- src/main/java/com/zenfulcode/commercify/commercify/entity/UserEntity.java
+++ src/main/java/com/zenfulcode/commercify/commercify/entity/UserEntity.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Table(name = "users")
+@Table(name = "Users")
 @Entity
 @Builder
 @Data
