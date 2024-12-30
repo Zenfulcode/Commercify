dc5b325400e784dd447e21a9ddb45a3525c9963f
GustavH <gkhaavik@gmail.com>
2024-12-30 15:23:21 +0100

    fixing unauthenticated users aka guests, could make order requests (might rework this later as it might be unsecure)

--- src/main/java/com/zenfulcode/commercify/commercify/integration/mobilepay/MobilePayController.java
+++ src/main/java/com/zenfulcode/commercify/commercify/integration/mobilepay/MobilePayController.java
@@ -5,6 +5,7 @@ import com.zenfulcode.commercify.commercify.api.responses.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,6 +38,7 @@ public class MobilePayController {
         }
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/webhook")
     public ResponseEntity<String> handleWebhook(
             @RequestParam String paymentReference,
